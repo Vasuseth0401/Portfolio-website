0e312f374474a04a596ba30f75177994ef26f8dc
Vasuseth0401 <145681275+Vasuseth0401@users.noreply.github.com>
2024-10-10 01:28:39 +0530

    Update intro.js

--- intro.js
+++ intro.js
@@ -19,8 +19,7 @@ function intro() {
      <p className="intropara">I am Skilled Web Developer with experience in creating
       <br/>
       visually appealing and user friendly  websites.
-      <br/>
-      Front-end is the part that is accessible to the user only, It should be self-explanatory and must be user-friendly and designed. <br/>so to achieve this we have some basic languages which can be used to create interactive web pages. These are the basic needs for the creation of a web page.
+      
     </p> 
     
     <Link>
